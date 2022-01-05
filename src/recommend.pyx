d628939611ddc8cad4548adede72312a52ff3ecd
Michael Vrazitulis <m.vrazitulis@yahoo.de>
2022-01-05 17:31:22 +0100

    got cython and gcc compilation working

--- src/recommend.pyx
+++ src/recommend.pyx
@@ -1,6 +1,17 @@
+# cython: language_level=3, boundscheck=False, wraparound=False
+
 cimport cython
-@cython.boundscheck(False)  # Deactivate bounds checking
-@cython.wraparound(False)
 
 
-# TODO
+def main():
+    cdef int test_int = 3
+    cdef int banana = 4
+
+    print(test_int)
+    print(banana)
+
+    # TODO
+
+
+if __name__ == '__main__':
+    main()
