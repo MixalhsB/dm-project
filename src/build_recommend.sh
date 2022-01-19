734e92d6f1ca199def4ebe081b76903e871c5f30
Michael Vrazitulis <m.vrazitulis@yahoo.de>
2022-01-19 23:47:09 +0100

    final shit

--- src/build_recommend.sh
+++ src/build_recommend.sh
@@ -1,2 +1,3 @@
 cython3 --embed recommend.pyx -o recommend.c
 gcc -Os -w -I /usr/include/python3.8 -I /home/michael/.local/lib/python3.8/site-packages/numpy/core/include recommend.c -lpython3.8 -o ../bin/recommend
+# modify second line to point to your own local python-include path and numpy-include path (see ../README.txt)
